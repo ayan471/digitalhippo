d1c22500004fadf7c727c67c89efbf2ee8096fe5
ayan471 <pakhiraayan@gmail.com>
2023-12-05 23:36:14 +0530

    final changes

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -12,11 +12,18 @@ type Category = (typeof PRODUCT_CATEGORIES)[number];
 interface NavItemProps {
   category: Category;
   handleOpen: () => void;
+  close: () => void;
   isOpen: boolean;
   isAnyOpen: boolean;
 }
 
-const NavItem = ({ isAnyOpen, category, handleOpen, isOpen }: NavItemProps) => {
+const NavItem = ({
+  isAnyOpen,
+  category,
+  handleOpen,
+  close,
+  isOpen,
+}: NavItemProps) => {
   return (
     <div className="flex">
       <div className="relative flex items-center">
@@ -33,12 +40,14 @@ const NavItem = ({ isAnyOpen, category, handleOpen, isOpen }: NavItemProps) => {
           />
         </Button>
       </div>
+
       {isOpen ? (
         <div
+          onClick={() => close()}
           className={cn(
             "absolute inset-x-0 top-full text-sm text-muted-foreground",
             {
-              "animate-spin fade-in-10 slide-in-from-top-5": !isAnyOpen,
+              "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen,
             }
           )}
         >
@@ -46,14 +55,16 @@ const NavItem = ({ isAnyOpen, category, handleOpen, isOpen }: NavItemProps) => {
             className="absolute inset-0 top-1/2 bg-white shadow"
             aria-hidden="true"
           />
+
           <div className="relative bg-white">
             <div className="mx-auto max-w-7xl px-8">
               <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
                 <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
                   {category.featured.map((item) => (
                     <div
+                      onClick={() => close}
                       key={item.name}
-                      className="group relative text- sm:text-sm"
+                      className="group relative text-base sm:text-sm"
                     >
                       <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
                         <Image
@@ -63,6 +74,7 @@ const NavItem = ({ isAnyOpen, category, handleOpen, isOpen }: NavItemProps) => {
                           className="object-cover object-center"
                         />
                       </div>
+
                       <Link
                         href={item.href}
                         className="mt-6 block font-medium text-gray-900"
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -20,7 +20,9 @@ export interface Config {
 }
 export interface User {
   id: string;
-  role?: ('admin' | 'user') | null;
+  products?: (string | Product)[] | null;
+  product_files?: (string | ProductFile)[] | null;
+  role: 'admin' | 'user';
   updatedAt: string;
   createdAt: string;
   email: string;
